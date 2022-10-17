b8db06a02a45937b2ddad2289a4bf16afab9fe66
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-17 07:56:30 +0300

    Setup the project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  root 'users#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
