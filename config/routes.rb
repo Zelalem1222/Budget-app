56195f703ad371988a55c2a698f72d6e0a679ec9
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-18 17:06:47 +0300

    remove users route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories do
     resources :purchases
   end
-  resources :users
+
   devise_for :users
   root 'categories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
