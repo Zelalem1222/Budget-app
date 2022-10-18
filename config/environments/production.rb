b8adf08b4a94485f2bb3c500abafbcf16f4c0bfb
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-18 16:58:56 +0300

    Add letter opener to production just incase it works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
