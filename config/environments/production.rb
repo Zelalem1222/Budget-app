7227216d4e2fc94b2e583b8b975760209aabcd13
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-18 12:39:48 +0300

    Change productionfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'myherokuapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://budget-controller1.herokuapp.com/' }
 end
