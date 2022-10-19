544557cd7aeaab3bb613c87105136472d50d5d3b
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-19 07:33:36 +0300

    Add the navbar to registration page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,10 +21,11 @@ class CategoriesController < ApplicationController
   # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
+    @category.user_id = current_user.id
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
