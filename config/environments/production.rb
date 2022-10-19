c620eee25d3c4003a7c85ad7a3e1fe3c0ed749ce
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-19 09:08:32 +0300

    Add mailer url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'budget-controller1.herokuapp.com' }
 end
