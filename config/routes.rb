320d5ed4733541efd82566902d57daa3aadf7cd8
Zelalem1222 <zelalemfissha394@gmail.com>
2022-11-18 08:57:30 +0300

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :purchases
   end
 
-  devise_for :users
+  # devise_for :users
   root 'categories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
