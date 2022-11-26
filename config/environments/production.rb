53014bf05d7be5ec0e85302bc620b9f648b4badc
Zelalem1222 <zelalemfissha394@gmail.com>
2022-11-26 08:08:45 +0300

    Make changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
